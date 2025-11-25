419c53c7f5442f8f5ed11639ae99be75f0c1c836
김현지 (Evelyn.Kim) <eve0204eve@gmail.com>
2025-11-25 14:57:14 +0900

    🔧 config: add Webpack rule to include image assets in build output
    - Added asset/resource rule to ensure images are copied to the dist folder
    - Prevented broken image paths when deploying with dist-only uploads
    - Improved build stability and deployment reliability for production environments

--- webpack.config.js
+++ webpack.config.js
@@ -5,14 +5,18 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',      // JS 엔트리
+  entry: './src/index.js',
+
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'js/bundle.[contenthash].js',
-    assetModuleFilename: 'assets/[name][ext]',
+    // 기본 이미지 출력 경로 역할
+    assetModuleFilename: 'assets/[name][ext]', 
   },
+
   module: {
     rules: [
+      // SCSS
       {
         test: /\.(sa|sc|c)ss$/,
         use: [
@@ -21,24 +25,33 @@ module.exports = {
           'sass-loader',
         ],
       },
+
+      // 이미지
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
         type: 'asset/resource',
+        generator: {
+          // dist/assets/로 복사 역할
+          filename: 'assets/[name][ext]', 
+        },
       },
     ],
   },
+
   plugins: [
     new CleanWebpackPlugin(),
+
+    // CSS 파일 추출 플러그인 (한 번만!)
     new MiniCssExtractPlugin({
       filename: 'css/styles.[contenthash].css',
     }),
+
+    // HTML 템플릿
     new HtmlWebpackPlugin({
-      template: './src/index.html',   // 기존 index.html을 템플릿으로 사용
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'main.css',
+      template: './src/index.html',
     }),
   ],
+
   devServer: {
     static: {
       directory: path.resolve(__dirname, 'dist'),
